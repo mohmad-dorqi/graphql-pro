68817ff242f5e4a3067c03a591849def63fe365d
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2022-09-03 10:07:55 +0430

    install graphql and apollo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { gql,useQuery } from '@apollo/client';
+
+const QUERY= gql`
+query {
+  articles {
+    id
+    text {
+      text
+    }
+  }
+}
+
+
+`
 function App() {
+  const {data}= useQuery(QUERY)
+  console.log(data);
   return (
     <div className="App">
-      app
+     {
+       data.articles.map(art => <div key={art.id}>
+        <p>{art.text.text}</p>
+       </div>
+        
+        
+        
+        )
+     }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient,ApolloProvider,InMemoryCache } from '@apollo/client';
 import './index.css';
 import App from './App';
 
-
+const client = new ApolloClient({
+  uri:"https://api-us-east-1.hygraph.com/v2/cl7ldser116ku01uhgbip4zid/master",
+  cache: new InMemoryCache(),
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+  <ApolloProvider client={client}>
+      <App />
+  </ApolloProvider>
+  
 );
 
