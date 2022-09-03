fdf86cafa058939bcef06f0ba5104c957c1d85e1
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2022-09-03 10:22:29 +0430

    install metrial ui

--- src/App.js
+++ src/App.js
@@ -2,32 +2,13 @@
 import './App.css';
 import { gql,useQuery } from '@apollo/client';
 
-const QUERY= gql`
-query {
-  articles {
-    id
-    text {
-      text
-    }
-  }
-}
 
-
-`
 function App() {
   const {data}= useQuery(QUERY)
   console.log(data);
   return (
     <div className="App">
-     {
-       data.articles.map(art => <div key={art.id}>
-        <p>{art.text.text}</p>
-       </div>
-        
-        
-        
-        )
-     }
+    
     </div>
   );
 }
