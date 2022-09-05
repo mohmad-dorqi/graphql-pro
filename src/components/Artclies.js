e27e6635451b314c28049412738d05f8035a9adb
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2022-09-05 11:37:30 +0430

    create articles

--- src/components/Artclies.js
+++ src/components/Artclies.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { GET_ARTICLES } from '../graphql/queryes';
 import { useQuery } from '@apollo/client';
+import Cart from '../shared/Card';
+import { Grid } from '@mui/material';
 
 const Artclies = () => {
     const {loading,data,error}= useQuery(GET_ARTICLES)
     console.log(data);
     return (
-        <div>
-           {
-           loading?<h1>loading</h1>  :data.articles.map(article => <div key={article.id}>
-               
-            </div>)
+       <Grid container spacing={2}>
+                 {
+           loading?<h1>loading</h1>  :data.articles.map(article => ( 
+           <Grid item xs={12} sm={6} md={4} key={article.id}>
+                  <Cart {...article}/>
+                 
+           </Grid>
 
-           }
-        </div>
+           ) )}
+
+       </Grid>
     );
 };
 
---
+++ src/shared/Card.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Divider, Typography } from '@mui/material';
+
+const Cart = ({athor,slug,photo,writer}) => {
+    console.log(athor.photo[0].url);
+    return (
+       <Card  sx={{boxShadow:'rgba(0,0,0,0.1) 0px 4px 12px', borderRadius:4}} >
+        <CardHeader
+        avatar={
+            <Avatar src={athor.photo[0].url} sx={{marginLift:2}}/>}
+            title={
+                <Typography component='p' variant='p' color='text.secondary'>
+                            {athor.name}
+                </Typography>
+            }
+        />
+        <CardMedia  component='img' height='194' image={photo[0].url} alt={slug}/>
+        <CardContent>
+            <Typography component='h3' variant='h6' color='text.primary'>
+                {writer  }
+            </Typography>
+        </CardContent>
+        <Divider variant='middle' sx={{margin:'10px'}}/>
+        <CardActions>
+            <Button variant='outlined' size='small' sx={{width:'100%',borderRadius: 3}}>Read Artclie</Button>
+        </CardActions>
+
+       </Card>
+    );
+};
+
+export default Cart;
