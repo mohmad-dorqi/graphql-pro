8d9104bfcabd72983a21df1054f99d17654c974e
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2022-09-05 09:29:50 +0430

    create get articles

--- src/graphql/queryes.js
+++ src/graphql/queryes.js
@@ -1,6 +1,27 @@
 import { gql } from "@apollo/client";
 
-const GET_ARTCLES= gql`
-
+const GET_ARTICLES= gql`
+query  {
+  articles {
+    id
+    slug
+    photo {
+      url
+    }
+    writer
+    athor {
+      ... on Athor {
+        id
+        name
+        photo {
+          url
+        }
+      }
+    }
+  }
+}
 
 `
+
+
+export {GET_ARTICLES};
