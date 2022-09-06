5c74ad9f2388a0f9b0e63f5527896c08c7cd5cf4
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2022-09-06 22:26:06 +0430

    get author info and set in author page

--- src/pages/AuthorPage.js
+++ src/pages/AuthorPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import { GET_AUTHOR_INFO } from '../graphql/queryes';
+import { Avatar, Container, Grid, Typography } from '@mui/material';
 
 
 
@@ -11,10 +12,20 @@ const AuthorPage = () => {
     variables:{slug},
    });
    console.log(data);
+   if(loading)  return <h1>Loading</h1>
     return (
-        <div>
-            
-        </div>
+        <Container  maxWidth='lg' >
+            <Grid container mt={10}>
+                <Grid item display='flex' flexDirection='column' alignItems='center' xs={12}>
+                  <Avatar src={data.athor.photo[0].url} sx={{width:'250px',height:'250px'}}/>
+                  <Typography component='h3' variant='h3' mt={4}>{data.athor.name}</Typography>
+                </Grid>
+                <Grid item xs={12} mt={7}>
+                    {data.athor.about.text}
+
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
