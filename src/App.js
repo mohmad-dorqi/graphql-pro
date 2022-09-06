0f33c40476c6dcd138d1a8890565547c4d8b6b34
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2022-09-06 18:50:19 +0430

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 
+import { Routes,Route } from 'react-router-dom';
+import ArticlePage from './pages/ArticlePage';
+import AuthorPage from './pages/AuthorPage';
 import './App.css';
 import Layout from './layout';
 import Home from './pages/Home';
@@ -10,7 +13,11 @@ function App() {
  
   return (
    <Layout>
-     <Home />
+     <Routes>
+         <Route path='/' element={<Home/>}/>
+         <Route path='/articles/:slug' element={<ArticlePage/>}/>
+         <Route path='/author/:slug' element={<AuthorPage/>}/>
+     </Routes>
    </Layout>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ApolloClient,ApolloProvider,InMemoryCache } from '@apollo/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const client = new ApolloClient({
   uri:"https://api-us-east-1.hygraph.com/v2/cl7ldser116ku01uhgbip4zid/master",
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <ApolloProvider client={client}>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
   </ApolloProvider>
   
 );
