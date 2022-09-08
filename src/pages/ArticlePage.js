1c2545e41c104fa2ca7d10e681500174e10440bb
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2022-09-08 10:24:04 +0430

    create upload photo

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,24 +1,13 @@
- import React,{useState} from 'react';
-import { getPhotoId, uploadPhoto } from '../helpers/uploadPhoto';
+ import React from 'react';
+
  
  const ArticlePage = () => {
-    const [file,setFile]=useState();
-    const changeHandler=async(event)=>{
-        
-     await  setFile(event.target.files[0]);
-     
-    }
-   const send=  ()=>{
-       uploadPhoto(file);
-
-     console.log(getPhotoId()); 
-   }
+   
 
     return (
         <div>
             
-            <input onChange={changeHandler} type="file"  name="filename"/>
-            <button onClick={send} >upload</button>
+            
            
         </div>
     );
