ffdfaf421864b2af6cbab945223824e94be136bf
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2022-09-10 20:43:13 +0430

    edit articlepage desgan

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,23 +1,55 @@
  import React from 'react';
- import { useParams } from 'react-router-dom';
+ import { useNavigate, useParams } from 'react-router-dom';
  import { useQuery } from '@apollo/client';
 import { GET_ARTICLE_INFO, GET_AUTHOR_INFO } from '../graphql/queryes';
+import { Avatar, Container, Grid, Typography } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 
  
  const ArticlePage = () => {
    
     const {slug}=useParams();
+    const navigate=useNavigate()
     const{data,loading,error}= useQuery(GET_ARTICLE_INFO,{
      variables:{slug},
     });
     console.log({data,loading,error});
+
+    if(loading) return <h1>loading...</h1>
     return (
-        <div>
-            
-          
-           
-        </div>
+       <Container maxWidth='lg' >
+           <Grid container >
+              <Grid item xs={12} mt={4} >
+                 <ArrowBackIcon onClick={()=>navigate(-1)} />
+              </Grid>
+              <Grid item xs={12} mt={4}>
+                 {
+                    data &&<img src={data.article.photo[0].url} width='100%' height='70%'/>
+                 }
+              </Grid>
+              <Grid item xs={12} display='flex'>
+                  <Avatar src={data.artcle.author.photo[0].url}/>
+                 
+              </Grid>
+              <Grid item xs={12} >
+                <Typography component='h3' variant='h5' mb={3}>
+                    {data.article.writer}
+                </Typography>
+                 
+              </Grid>
+              <Grid item xs={12} mt={4}>
+                <Typography component='p' variant='p'>
+                    {data.article.text.text}
+
+                </Typography>
+
+              </Grid>
+
+           </Grid>
+
+
+       </Container>
     );
  };
  
