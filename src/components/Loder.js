7cfff5f5d327a669b464e805808dbe5787e261ac
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2022-09-10 21:30:16 +0430

    styled loader component

--- src/components/Loder.js
+++ src/components/Loder.js
@@ -1,9 +1,19 @@
+import { display, height, padding } from '@mui/system';
 import React from 'react';
 import loader  from '../assets/image/Doctors symbol.gif'
 
 const Loder = () => {
     return (
-        <div>
+        <div style={{
+            width:'100%',
+            height:'100px',
+            display:'flex',
+            justifyContent:'center',
+            paddingTop:'40px',
+            marginBottom:'250px',
+            marginTop:'150px'
+
+        }}>
             <img src={loader} alt='loader'/>
             
         </div>
